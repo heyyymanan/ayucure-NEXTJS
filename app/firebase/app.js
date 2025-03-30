993854cf798755cbd4697d4e819de2e8cc98eb0f
heyymanan <mananvyas50@gmail.com>
2025-03-30 20:58:32 +0530

    new_unFroze_pnpm

--- app/firebase/app.js
+++ app/firebase/app.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,11 +13,11 @@ const firebaseConfig = {
   storageBucket: "bynatablet-in.firebasestorage.app",
   messagingSenderId: "1059784443046",
   appId: "1:1059784443046:web:c73177ae690f29c41be067",
-  measurementId: "G-CMKSRTSM2D"
+  // measurementId: "G-CMKSRTSM2D"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 export {app};
