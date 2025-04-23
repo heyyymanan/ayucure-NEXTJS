c4fdf0977e69fe4f184b2f6df09e4bba80bd692a
heyymanan <mananvyas50@gmail.com>
2025-04-23 18:20:59 +0530

    product_card_responsive final

--- components/ui/product_card.jsx
+++ components/ui/product_card.jsx
@@ -51,34 +51,34 @@ const ProductCard = ({ product }) => {
 
   return (
     <div
-      
-      className="border rounded-2xl shadow-md max-w-xs sm:max-w-sm md:max-w-md bg-white px-4 pb-3 sm:p-5 transition-transform hover:scale-105 hover:cursor-pointer"
+
+      className="border rounded-2xl  shadow-md max-w-xs h-auto sm:max-w-sm md:max-w-md bg-white p-1 pb-3 sm:p-4 transition-transform hover:scale-105 hover:cursor-pointer"
     >
-      <div className="relative flex justify-center w-full h-56 sm:h-64">
+      <div className="relative flex justify-center w-full h-auto sm:h-64">
         <Image
           src={product.images[0]}
           alt={product.name}
-          width={255}
+          width={200}
           height={200}
-          className="rounded-lg object-contain"
+          className="rounded-lg object-contain w-[187px] h-[187px] sm:w-[230px] sm:h-[230px]"
         />
       </div>
 
-      <div className="mt-1">
-        <div className="flex justify-between items-center">
+      <div className="mt-1 sm:space-y-4">
+        <div className="flex  justify-between items-center">
           {product.variants[0].discount > 0 && (
             <span className="bg-blue-100 text-blue-700 text-xs font-semibold px-2 py-1 rounded-full">
               {product.variants[0].discount}% OFF
             </span>
           )}
-          <span className="bg-slate-200 text-black text-xs font-semibold px-2 py-1 rounded-full">
+          <span className="bg-slate-200 text-black sm:text-xs text-[10px] font-semibold px-2 py-1 ml-1  rounded-full">
             {product.variants[0].size}
           </span>
         </div>
 
-        <h3 className="mt-3 text-lg sm:text-xl font-bold text-gray-900">{product.name}</h3>
+        <h3 className="mt-1 text-base ml-1 sm:text-xl font-bold text-gray-900">{product.name}</h3>
 
-        <div className="flex items-center mt-2">
+        <div className="flex h-4 sm:h-auto items-center mx-1 mt-2">
           {[...Array(fullStars)].map((_, i) => (
             <Star key={`full-${i}`} className="text-yellow-500 w-5 h-5" />
           ))}
@@ -96,45 +96,51 @@ const ProductCard = ({ product }) => {
             {product.remedy_for.map((remedy, idx) => (
               <span
                 key={idx}
-                className="text-xs sm:text-sm text-gray-700 bg-gray-100 rounded-full px-2 py-1"
+                className="text-xs sm:text-sm mx-1 text-gray-700 bg-gray-100 rounded-full px-2 py-1"
               >
                 💊 {remedy}
               </span>
             ))}
           </div>
         ) : null}
+        <div className="btm flex items-center mt-2 sm:gap-3 justify-around">
 
-        <div className="flex justify-between items-center mt-4">
-          <span className="text-2xl font-semibold text-gray-900">
-            ₹{product.variants[0].price}
-          </span>
-        </div>
-
-        {cartItem ? (
-          <div ref={buttonRef} className="flex border-2 py-2 border-lime-500 rounded-lg  items-center justify-evenly  mt-4 sm:gap-4">
-            <button onClick={handleDecrement} className="text-gray-700 hover:text-gray-900">
-              <LucideMinusCircle size={24} />
-            </button>
-            <span className="text-lg font-medium">{cartItem.quantity}</span>
-            <button onClick={handleIncrement} className="text-gray-700 hover:text-gray-900">
-              <LucidePlusCircle size={24} />
-            </button>
+          <div className="flex mx-1 items-center">
+            <span className="text-2xl sm:text-3xl font-semibold text-gray-900">
+              ₹{product.variants[0].price}
+            </span>
           </div>
-        ) : (
-          <button
-            onClick={handleAddToCart}
-            className="w-full mt-4 p-3 bg-lime-500 text-white flex items-center justify-center rounded-lg hover:bg-blue-700 transition-all"
-          >
-            <HugeiconsIcon
-              icon={PlusSignCircleFreeIcons}
-              size={20}
-              className="mr-2 hidden sm:block"
-              color="currentColor"
-              strokeWidth={1}
-            />
-            <span>Add to Cart</span>
-          </button>
-        )}
+
+          {cartItem ? (
+            <div ref={buttonRef} className="flex border-2 px-2 py-2 border-lime-500 rounded-lg  items-center justify-evenly gap-3 sm:gap-4">
+              <button onClick={handleDecrement} className="text-gray-700 mx-1 hover:text-gray-900">
+                <LucideMinusCircle size={24} />
+              </button>
+              <span className="sm:text-lg text-base font-medium">{cartItem.quantity}</span>
+              <button onClick={handleIncrement} className="text-gray-700 mx-1 hover:text-gray-900">
+                <LucidePlusCircle size={24} />
+              </button>
+            </div>
+          ) : (
+            <div className="addToCart w-full flex justify-center mt-1">
+
+              <button
+                onClick={handleAddToCart}
+                className="px-2 py-2 sm:text-xl bg-lime-500 text-white flex items-center justify-center rounded-lg hover:bg-blue-700 transition-all"
+              >
+                <HugeiconsIcon
+                  icon={PlusSignCircleFreeIcons}
+                  size={20}
+                  className="mr-2 hidden sm:block"
+                  color="currentColor"
+                  strokeWidth={1}
+                />
+                <span>Add to Cart</span>
+              </button>
+
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
