6e25c821955e43c717daffdd35777658f282068e
heyymanan <mananvyas50@gmail.com>
2025-07-12 22:25:05 +0530

    ..

--- app/checkout/[SessionId]/page.jsx
+++ app/checkout/[SessionId]/page.jsx
@@ -195,7 +195,7 @@ export default function CheckoutPage() {
             );
 
             data = response.data;
-            console.log(data)
+            
             
 
         } catch (error) {
